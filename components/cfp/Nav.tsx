698122849ee90c51153665803074d397b7caa71c
gnehs <jayuiop@gmail.com>
2023-10-26 14:51:08 +0800

    updated cfp nav

--- components/cfp/Nav.tsx
+++ components/cfp/Nav.tsx
@@ -1,7 +1,41 @@
+"use client";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import { usePathname } from "next/navigation";
+function NavLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  const isActive = usePathname() === href;
+  return (
+    <Link
+      href={href}
+      className={twMerge(
+        "flex gap-2 transition-colors hover:font-bold text-[#1E1E1E]",
+        isActive && "font-bold"
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
+
 export default function Nav() {
   return (
-    <nav className="h-16 w-full bg-[#E5C366]">
-      <div className="container">nav</div>
+    <nav className="w-full bg-[#E5C366]">
+      <div className="container flex items-center justify-start gap-6 h-16">
+        <img
+          src="/sitcon_logo.svg"
+          alt="SITCON Logo"
+          className="-translate-y-1 h-10 mr-2"
+        />
+        <NavLink href="/cfp">首頁</NavLink>
+        <NavLink href="/cfp/submit">議程與徵稿</NavLink>
+      </div>
     </nav>
   );
 }
