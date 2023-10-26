5f42256d27c200e4207e3b90d1c4db746e4871f6
gnehs <jayuiop@gmail.com>
2023-10-26 18:19:53 +0800

    added fortawesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+// fontawesome
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 export const metadata: Metadata = {
   title: "SITCON 2024 CFP",
