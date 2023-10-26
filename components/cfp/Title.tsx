844438c86c824c82d6b21b6a86750bf0f7fd5bb1
Dada878 <xx0932399@gmail.com>
2023-10-26 13:54:22 +0800

    create title component

---
+++ components/cfp/Title.tsx
@@ -0,0 +1,6 @@
+export default function Title(props: {content:string}) {
+    return (
+      <h1 className="text-5xl leading-[75px] border-b-[#E5C366] border-b pt-2 font-bold mb-5">{props.content}</h1>
+    );
+  }
+  
