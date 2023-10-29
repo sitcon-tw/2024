ac7a9eb184f8a0e27fa96156787f5ec7c4af1c15
Dada878 <xx0932399@gmail.com>
2023-10-29 19:18:20 +0800

    updated coc page

--- app/cfp/(submit)/coc/page.tsx
+++ app/cfp/(submit)/coc/page.tsx
@@ -1,9 +1,11 @@
 import Title from "@/components/cfp/Title";
 export default function Page() {
-  return <div className="container">
-    <Title content="CODE OF CONDUCT"></Title>
-    <div className="prose prose-invert lg:prose-xl leading-8">
-      <p className="my-2">SITCON 歡迎不同身分、來自不同背景的與會者，我們鼓勵女性、性少數與多元背景的參與者。為了讓大家都能愉快地參加 SITCON，我們要求所有參與者閱讀年會的 <a className="text-blue-600" target="_blank" href="https://sitcon.org/code-of-conduct/">行為準則（Code of Conduct）</a>，共同創造一個友善的環境。</p>
+  return (
+    <div className="flex flex-col gap-6">
+      <Title content="CODE OF CONDUCT"></Title>
+      <div className="prose prose-invert lg:prose-xl leading-8">
+        <p>SITCON 歡迎不同身分、來自不同背景的與會者，我們鼓勵女性、性少數與多元背景的參與者。為了讓大家都能愉快地參加 SITCON，我們要求所有參與者閱讀年會的 <a className="text-blue-600" target="_blank" href="https://sitcon.org/code-of-conduct/">行為準則（Code of Conduct）</a>，共同創造一個友善的環境。</p>
+      </div>
     </div>
-  </div>;
+  );
 }
