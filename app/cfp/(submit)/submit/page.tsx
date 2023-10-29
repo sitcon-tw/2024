fd00520534042b3aba316dc620d7ee8acaef68be
SimbaFs <simba.fs@gmail.com>
2023-10-29 22:15:36 +0800

    finish countdown in submit page

--- app/cfp/(submit)/submit/page.tsx
+++ app/cfp/(submit)/submit/page.tsx
@@ -1,52 +1,86 @@
-"use client"
+'use client'
 import Title from '@/components/cfp/Title'
 import Event, { EventType } from '@/components/cfp/Event'
-import { useReducer } from 'react'
+import { useEffect, useReducer, useState } from 'react'
+
+function Countdown() {
+  const [deadline] = useState(new Date('2024-01-20T23:59:00+08:00'))
+  const [days, setDays] = useState(99)
+  const [hours, setHours] = useState(99)
+  const [minutes, setMinutes] = useState(99)
+  const [seconds, setSeconds] = useState(99)
+
+  useEffect(() => {
+    let diff = (deadline.getTime() - Date.now()) / 1000
+
+    if (diff <= 0) {
+      return
+    }
+
+    const interval = setInterval(() => {
+      setSeconds(Math.floor(diff % 60))
+      diff /= 60
+      setMinutes(Math.floor(diff % 60))
+      diff /= 60
+      setHours(Math.floor(diff % 24))
+      diff /= 24
+      setDays(Math.floor(diff))
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [days, hours, minutes, minutes, seconds, deadline])
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <p>距離投稿截止還有：</p>
+      <div className="w-full border-[#E5C366] border-2 rounded-full py-[50px] px-[100px] grid grid-cols-4">
+        <span className="text-[#FF3495] text-6xl text-center">
+          {days}
+        </span>
+        <span className="text-[#46A5FD] text-6xl text-center">
+          {hours}
+        </span>
+        <span className="text-[#0CE295] text-6xl text-center">
+          {minutes}
+        </span>
+        <span className="text-[#AC24FF] text-6xl text-center">
+          {seconds}
+        </span>
+        <span className="font-medium text-[#E5C366] text-center">
+          DAYS
+        </span>
+        <span className="font-medium text-[#E5C366] text-center">
+          HOURS
+        </span>
+        <span className="font-medium text-[#E5C366] text-center">
+          MINUTES
+        </span>
+        <span className="font-medium text-[#E5C366] text-center">
+          SECONDS
+        </span>
+      </div>
+      <p className="text-sm font-bold">截止日期</p>
+      <p>即日起至 2024 年 1 月 20 日（六）23:59</p>
+    </div>
+  )
+}
 
 export default function Page() {
-  const [type, toggleTyoe] = useReducer((state: EventType | '', action: EventType | '') => {
-    if (state === '') return action
-    if (state === action) return ''
-    else return action
-  }, '')
+  const [type, toggleTyoe] = useReducer(
+    (state: EventType | '', action: EventType | '') => {
+      if (state === '') return action
+      if (state === action) return ''
+      else return action
+    },
+    ''
+  )
 
   return (
     <div className="flex flex-col gap-6">
       <Title content="投稿頁面" />
 
       {/* count down */}
-      <div className="flex flex-col items-center gap-4">
-        <p>距離投稿截止還有：</p>
-        <div className="w-full border-[#E5C366] border-2 rounded-full py-[50px] px-[100px] grid grid-cols-4">
-          <span className="text-[#FF3495] text-6xl text-center">
-            99
-          </span>
-          <span className="text-[#46A5FD] text-6xl text-center">
-            99
-          </span>
-          <span className="text-[#0CE295] text-6xl text-center">
-            99
-          </span>
-          <span className="text-[#AC24FF] text-6xl text-center">
-            99
-          </span>
-          <span className="font-medium text-[#E5C366] text-center">
-            DAYS
-          </span>
-          <span className="font-medium text-[#E5C366] text-center">
-            HOURS
-          </span>
-          <span className="font-medium text-[#E5C366] text-center">
-            MINUTES
-          </span>
-          <span className="font-medium text-[#E5C366] text-center">
-            SECONDS
-          </span>
-        </div>
-        <p className="text-sm font-bold">截止日期</p>
-        <p>即日起至 2024 年 1 月 20 日（六）23:59</p>
-      </div>
-
+      <Countdown />
       {/* submit botton */}
       <div className="flex justify-center">
         <button className="bg-[#E5C366] text-white h-[60px] w-[196px] rounded-full">
