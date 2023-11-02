e8cb1c3d27d6042cc10faa87ff7b6ca0dde60e12
gnehs <jayuiop@gmail.com>
2023-11-02 16:43:21 +0800

    added about & visual component

---
+++ components/cfp/About.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <h1>關於 SITCON</h1>
+    </div>
+  );
+}
---
+++ components/cfp/Visual.tsx
@@ -0,0 +1,10 @@
+export default function Visual() {
+  return (
+    <div
+      className="h-[100svh] bg-white"
+      style={{
+        backgroundImage: `url('/intro.webp')`,
+      }}
+    ></div>
+  );
+}
