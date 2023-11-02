e5200199a312f1fcd5ce2e7628367ed355b99cd1
gnehs <jayuiop@gmail.com>
2023-11-02 18:18:53 +0800

    updated about component

--- components/cfp/About.tsx
+++ components/cfp/About.tsx
@@ -1,7 +1,27 @@
 export default function About() {
   return (
-    <div>
-      <h1>關於 SITCON</h1>
+    <div className="relative my-10">
+      <img
+        src="/hero_bg.webp"
+        className="absolute top-0 bottom-0 right-0 w-[50vw] -z-10"
+      />
+      <div className="container relative py-10">
+        <div className="w-full lg:w-[500px]">
+          <h1 className="text-4xl font-bold mb-4">關於 SITCON</h1>
+          <div>
+            學生計算機年會（Students’ Information Technology Conference）自 2013
+            年發起，以學生為本、由學生自發舉辦，長期投身學生資訊教育與推廣開源精神，希望引領更多學子踏入資訊的殿堂，更冀望所有對資訊有興趣的學生，能夠在年會裏齊聚一堂，彼此激盪、傳承、啟發，達到「學以致用、教學相長」的實際展現。
+          </div>
+          <h1 className="text-4xl font-bold mb-4 mt-16">參與討論</h1>
+          <div>
+            SITCON
+            是個開放的社群，許多籌備的相關事項都會在公開的郵件論壇上進行討論，如果有興趣參與，歡迎訂閱
+            SITCON 的郵件論壇並貢獻你的想法！
+            若你想進一步了解過往討論的內容，也歡迎到 GitLab 上查看 SITCON 2024
+            的籌備歷程。
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
