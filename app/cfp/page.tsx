b073c761ce7eeeabb3c158d66cf58c23cd37d122
gnehs <jayuiop@gmail.com>
2023-11-03 09:37:21 +0800

    updated /cfp

--- app/cfp/page.tsx
+++ app/cfp/page.tsx
@@ -3,62 +3,37 @@ import BackToTopButton from "@/components/BackToTopBtn";
 import Sponsorship from "@/components/cfp/Sponsorship";
 import React, { useState, useEffect } from "react";
 import About from "@/components/cfp/About";
-import Counter from "@/components/cfp/Counter";
 import Visual from "@/components/cfp/Visual";
+
+function TwoLineDivider() {
+  return (
+    <div className="mb-16 flex flex-row	items-center justify-center gap-[10px]">
+      <div className="flex flex-col items-center h-[600px]">
+        <div className="w-[5px] h-[5px] rounded-full bg-[gold]"></div>
+        <div className="w-[1px] h-[600px] bg-[gold]"></div>
+        <div className="w-[5px] h-[5px] rounded-full bg-[gold]"></div>
+      </div>
+      <div className="flex flex-col items-center h-[600px]">
+        <div className="w-[5px] h-[5px] rounded-full bg-[gold]"></div>
+        <div className="w-[1px] h-[600px] bg-[gold]"></div>
+        <div className="w-[5px] h-[5px] rounded-full bg-[gold]"></div>
+      </div>
+    </div>
+  );
+}
+function LineDivider() {
+  return (
+    <div className="mb-16 flex flex-row	items-center justify-center gap-[10px]">
+      <div className="flex flex-col items-center h-[300px]">
+        <div className="w-[5px] h-[5px] rounded-full bg-[gold]"></div>
+        <div className="w-[1px] h-[300px] bg-[gold]"></div>
+        <div className="w-[5px] h-[5px] rounded-full bg-[gold]"></div>
+      </div>
+    </div>
+  );
+}
+
 export default function Page() {
-  const statsContentText = (index: number) => {
-    switch (index) {
-      case 1:
-        return "超過";
-      case 2:
-        return "";
-      case 3:
-        return "";
-      case 4:
-        return "扶植近";
-      case 5:
-        return "接觸學生資訊人才";
-      case 6:
-        return "提升品牌影響力";
-      case 7:
-        return "深入學生群體";
-      case 8:
-        return "推廣產品內容";
-      default:
-        return "";
-    }
-  };
-
-  const statsContentCount = (index: number) => {
-    switch (index) {
-      case 1:
-        return <Counter value={10000} />;
-      case 2:
-        return <Counter value={350} />;
-      case 3:
-        return <Counter value={400} />;
-      case 4:
-        return <Counter value={1000} />;
-      default:
-        return "";
-    }
-  };
-
-  const statsContentUnit = (index: number) => {
-    switch (index) {
-      case 1:
-        return "名與會者";
-      case 2:
-        return "名學生講者";
-      case 3:
-        return "場聚會與講座";
-      case 4:
-        return "名資訊推廣志工";
-      default:
-        return "";
-    }
-  };
-
   const styles = {
     linesContainer: {
       display: "flex",
@@ -100,18 +75,7 @@ export default function Page() {
       <div className="container mx-auto">
         <div className="flex justify-center">
           <div className="w-1/2">
-            <div className="mb-16" style={styles.linesContainer}>
-              <div style={{ ...styles.singleLineContainer, height: "600px" }}>
-                <div style={styles.lineDot}></div>
-                <div style={{ ...styles.line, height: "600px" }}></div>
-                <div style={styles.lineDot}></div>
-              </div>
-              <div style={{ ...styles.singleLineContainer, height: "600px" }}>
-                <div style={styles.lineDot}></div>
-                <div style={{ ...styles.line, height: "600px" }}></div>
-                <div style={styles.lineDot}></div>
-              </div>
-            </div>
+            <TwoLineDivider />
             <div className="text-center text-2xl gap-2 leading-relaxed mb-16">
               從計算機自百年前誕生後，
               <br />
@@ -126,13 +90,7 @@ export default function Page() {
               <br />
               &#x2015;&#x2015;<span style={{ color: "gold" }}>有限</span>。
             </div>
-            <div className="mb-16" style={styles.linesContainer}>
-              <div style={styles.singleLineContainer}>
-                <div style={styles.lineDot}></div>
-                <div style={styles.line}></div>
-                <div style={styles.lineDot}></div>
-              </div>
-            </div>
+            <LineDivider />
             <div className="p-10">
               <h2 className="text-2xl mb-6 text-center">
                 SITCON 2024 年會主題
@@ -153,24 +111,12 @@ export default function Page() {
                 年的社會也不斷有各式問題需要解決：社群媒體審查機制、內容推薦演算法、科技綁架、個人資料追蹤、深度偽造技術、詐騙及假新聞、科技巨擎壟斷行為、開源文化打壓、各式資安議題等等。現今計算機、網路也為人類社會帶來了許多問題，每個議題的複雜程度之高難以讓學生們面對和解決。
               </p>
             </div>
-            <div className="mb-16" style={styles.linesContainer}>
-              <div style={styles.singleLineContainer}>
-                <div style={styles.lineDot}></div>
-                <div style={styles.line}></div>
-                <div style={styles.lineDot}></div>
-              </div>
-            </div>
+            <LineDivider />
             <div className="text-center text-2xl font-bold gap-2 leading-relaxed mb-16">
               但不要忘記，這些問題都皆出自於我們人類。 <br />
               而身為學生，我們一定有參與行動的方式。 <br />
             </div>
-            <div className="mb-4" style={styles.linesContainer}>
-              <div style={styles.singleLineContainer}>
-                <div style={styles.lineDot}></div>
-                <div style={styles.line}></div>
-                <div style={styles.lineDot}></div>
-              </div>
-            </div>
+            <LineDivider />
             <div className="p-10">
               <p className="leading-relaxed mb-4">
                 我們可以先從觀察問題開始，了解相關知識，並思考、研究、實作，找出解決問題的靈感；我們可以和身邊的同學討論，利用群眾智慧共同創造有意義的解決方案；我們可以成為實踐者，貢獻程式碼、打造並部署服務、透過實際行動推動專案；我們可以將這些結晶透過任何形式——甚至變成議程——分享給想認識相關議題的同學，讓更多人能關注你認為重要的議題，打造更緊密的社群。
@@ -182,13 +128,7 @@ export default function Page() {
                 日新月異的科技讓人類感覺似乎不再是人，也讓身為學生的我們對於學習逐漸失去意義。即便如此，人類最終是受好奇心驅使的動物，即便最後計算機發展到與人類無異，好奇心與熱情永遠將是與廣義上計算機最大的差異。
               </p>
             </div>
-            <div className="mb-16" style={styles.linesContainer}>
-              <div style={styles.singleLineContainer}>
-                <div style={styles.lineDot}></div>
-                <div style={styles.line}></div>
-                <div style={styles.lineDot}></div>
-              </div>
-            </div>
+            <LineDivider />
             <div className="text-center text-4xl font-bold gap-2 leading-relaxed mb-16">
               因為，我們終究是人類。
             </div>
