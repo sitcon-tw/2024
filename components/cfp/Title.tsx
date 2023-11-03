dce2ba3382c5a061500565428d507d1ad0f08998
Dada878 <xx0932399@gmail.com>
2023-11-04 05:56:34 +0800

    updated title component

--- components/cfp/Title.tsx
+++ components/cfp/Title.tsx
@@ -1,6 +1,5 @@
 export default function Title(props: {content:string}) {
-    return (
-      <h1 className="text-5xl leading-[75px] border-b-[#E5C366] border-b pt-2 font-bold">{props.content}</h1>
-    );
-  }
-  
+  return (
+    <h1 className="text-3xl leading-normal lg:text-5xl lg:leading-[75px] border-b-[#E5C366] border-b pt-2 font-bold">{props.content}</h1>
+  );
+}
