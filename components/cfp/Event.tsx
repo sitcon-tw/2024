d9ad6a281c81bb60ec70dcee5f1b6b3c2c88889e
SimbaFs <simba.fs@gmail.com>
2023-11-05 03:35:27 +0800

    update component Event

--- components/cfp/Event.tsx
+++ components/cfp/Event.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from "tailwind-merge";
+
 export type EventTime = [number, string, number | string];
 
 export type EventType = "general" | "undefined" | "poster" | "SITCON";
@@ -6,17 +8,53 @@ export type EventProp = {
   time: EventTime;
   // [2023, 'Nov.', 19]
   // [2023, 'Nov.', 下旬]
-
   endTime?: EventTime;
-
   type: EventType[];
-
   selectedType?: EventType | "";
-
   name: string;
-
   desc?: string;
 };
+
+function zero(n: number | string) {
+  if (typeof n === "string") return n;
+  if (n < 10) return `0${n}`;
+  return `${n}`;
+}
+
+function Time({ time, isGray }: { time: EventTime; isGray: boolean }) {
+  return (
+    <div className="flex h-min px-2 first:pt-2 last:pb-2">
+      <div className="flex flex-col items-center">
+        <div
+          className={twMerge(
+            "transition-colors",
+            isGray ? "text-1-6" : "text-4-6"
+          )}
+        >
+          {time[0]}
+        </div>
+        <div
+          className={twMerge(
+            "text-2xl transition-colors",
+            isGray ? "text-1-6" : "text-4-6"
+          )}
+        >
+          {time[1]}
+        </div>
+      </div>
+
+      <div
+        className={twMerge(
+          "flex flex-col justify-end items-center transition-colors",
+          typeof time[2] === "string" ? "text-2xl" : "text-5xl",
+          isGray ? "text-1-6" : "text-4-6"
+        )}
+      >
+        <div>{zero(time[2])}</div>
+      </div>
+    </div>
+  );
+}
 
 export default function Event({
   time,
@@ -26,25 +64,6 @@ export default function Event({
   name,
   desc,
 }: EventProp) {
-  function Time({ time }: { time: EventTime }) {
-    return (
-      <div className="flex h-min px-2 first:pt-2 last:pb-2">
-        <div className="flex flex-col items-center">
-          <div className="text-gold">{time[0]}</div>
-          <div className="text-gold text-2xl">{time[1]}</div>
-        </div>
-
-        <div
-          className={`text-gold flex flex-col justify-end items-center ${
-            typeof time[2] === "string" ? "text-2xl" : "text-5xl"
-          }`}
-        >
-          <div>{zero(time[2])}</div>
-        </div>
-      </div>
-    );
-  }
-
   const typeColor = {
     general: "bg-purple",
     undefined: "bg-pink",
@@ -52,49 +71,56 @@ export default function Event({
     SITCON: "bg-blue",
   };
 
-  function zero(n: number | string) {
-    if (typeof n === "string") return n;
-
-    if (n < 10) return `0${n}`;
-    return `${n}`;
-  }
-
-  const isGray =
-    selectedType === undefined ||
-    type.includes("SITCON") ||
-    selectedType === "" ||
-    type.includes(selectedType);
+  const isGray = !!(
+    selectedType &&
+    !type.includes("SITCON") &&
+    !type.includes(selectedType)
+  );
 
   return (
-    <div
-      className={`min-h-[110px] max-w-[512px] flex border border-1-6 rounded shadow-[0px_4px_8px_0px_#0000001A] mb-6 ${
-        isGray ? "" : "grayscale"
-      }`}
-    >
+    <div className="min-h-[110px] max-w-[512px] flex border border-1-6 rounded-lg shadow-[0px_4px_8px_0px_#0000001A] my-6 bg-white">
       <div className="flex flex-col">
         {type.map((type) => (
           <div
             key={type}
-            className={`${typeColor[type]} w-[5px] flex-grow first:rounded-tl-[3.5px] last:rounded-bl-[3.5px]`}
+            className={twMerge(
+              "w-[5px] flex-grow first:rounded-tl-[3.5px] last:rounded-bl-[3.5px] transition-colors",
+              typeColor[type],
+              isGray && "bg-1-6"
+            )}
           />
         ))}
       </div>
 
       <div>
-        <Time time={time} />
+        <Time time={time} isGray={isGray} />
 
         {endTime ? (
           <>
-            <div className="border border-white border-r-gold w-[94px] h-[37px]" />
-            <Time time={endTime} />
+            <div
+              className={twMerge(
+                "border border-white w-[94px] h-[37px] transition-colors",
+                isGray ? "border-r-1-6" : "border-r-4-6"
+              )}
+            />
+            <Time time={endTime} isGray={isGray} />
           </>
         ) : null}
       </div>
 
       {/* todo: align */}
       <div className="text-4-6 my-2 mx-4 flex flex-col">
-        <span className="text-[32px] font-bold">{name}</span>
-        <span>{desc}</span>
+        <span
+          className={twMerge(
+            "text-[32px] font-bold transition-colors",
+            isGray && "text-1-6"
+          )}
+        >
+          {name}
+        </span>
+        <span className={twMerge("transition-colors", isGray && "text-1-6")}>
+          {desc}
+        </span>
       </div>
     </div>
   );
