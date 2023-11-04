6a6e6a5aa3aa6cbbdff80cff1b00e85bd8b651e5
gnehs <jayuiop@gmail.com>
2023-11-05 00:13:26 +0800

    updated layout style

--- app/cfp/(submit)/layout.tsx
+++ app/cfp/(submit)/layout.tsx
@@ -37,8 +37,8 @@ export default function SubmitLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex gap-6 container">
-      <div className="min-w-[180px] hidden lg:flex flex-col gap-2 mt-6 sticky top-[88px] self-start">
+    <div className="flex gap-6 container mt-6">
+      <div className="min-w-[180px] hidden lg:flex flex-col gap-2 sticky top-[88px] self-start">
         <div className="text-[#0CE295] font-bold text-lg">議程與海報徵稿</div>
         <NavLink href="/cfp/submit">投稿頁面</NavLink>
         <NavLink href="/cfp/normal">一般議程</NavLink>
