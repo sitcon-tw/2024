57284fa30857eb06c92ec7ce434137dc5c5e9f77
SimbaFs <simba.fs@gmail.com>
2023-11-05 01:37:22 +0800

    fix my indent to 2 space

--- app/cfp/(submit)/submit/page.tsx
+++ app/cfp/(submit)/submit/page.tsx
@@ -1,171 +1,171 @@
-'use client'
-import { useReducer } from 'react'
-import Image from 'next/image'
+"use client";
+import { useReducer } from "react";
+import Image from "next/image";
 
-import Title from '@/components/cfp/Title'
-import Event, { EventType } from '@/components/cfp/Event'
+import Title from "@/components/cfp/Title";
+import Event, { EventType } from "@/components/cfp/Event";
 
-import useCountdown from '@/hooks/useCountdown'
+import useCountdown from "@/hooks/useCountdown";
 
 function Countdown() {
-	const { days, hours, minutes, seconds } = useCountdown()
-	return (
-		<div className="flex flex-col items-center gap-4">
-			<p>距離投稿截止還有：</p>
-			<div className="w-full border-gold border-2 rounded-full py-7 px-14 lg:py-[50px] lg:px-[100px] grid grid-cols-4">
-				<span className="text-pink text-[32px] lg:text-[64px] text-center">
-					{days}
-				</span>
-				<span className="text-purple text-[32px] lg:text-[64px] text-center">
-					{hours}
-				</span>
-				<span className="text-blue text-[32px] lg:text-[64px] text-center">
-					{minutes}
-				</span>
-				<span className="text-green text-[32px] lg:text-[64px] text-center">
-					{seconds}
-				</span>
-				<span className="font-medium text-[10px] lg:text-base text-gold text-center">
-					DAYS
-				</span>
-				<span className="font-medium text-[10px] lg:text-base text-gold text-center">
-					HOURS
-				</span>
-				<span className="font-medium text-[10px] lg:text-base text-gold text-center">
-					MINUTES
-				</span>
-				<span className="font-medium text-[10px] lg:text-base text-gold text-center">
-					SECONDS
-				</span>
-			</div>
-			<p className="text-sm font-bold">截止日期</p>
-			<p>即日起至 2024 年 1 月 20 日（六）23:59</p>
-		</div>
-	)
+  const { days, hours, minutes, seconds } = useCountdown();
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <p>距離投稿截止還有：</p>
+      <div className="w-full border-gold border-2 rounded-full py-7 px-14 lg:py-[50px] lg:px-[100px] grid grid-cols-4">
+        <span className="text-pink text-[32px] lg:text-[64px] text-center">
+          {days}
+        </span>
+        <span className="text-purple text-[32px] lg:text-[64px] text-center">
+          {hours}
+        </span>
+        <span className="text-blue text-[32px] lg:text-[64px] text-center">
+          {minutes}
+        </span>
+        <span className="text-green text-[32px] lg:text-[64px] text-center">
+          {seconds}
+        </span>
+        <span className="font-medium text-[10px] lg:text-base text-gold text-center">
+          DAYS
+        </span>
+        <span className="font-medium text-[10px] lg:text-base text-gold text-center">
+          HOURS
+        </span>
+        <span className="font-medium text-[10px] lg:text-base text-gold text-center">
+          MINUTES
+        </span>
+        <span className="font-medium text-[10px] lg:text-base text-gold text-center">
+          SECONDS
+        </span>
+      </div>
+      <p className="text-sm font-bold">截止日期</p>
+      <p>即日起至 2024 年 1 月 20 日（六）23:59</p>
+    </div>
+  );
 }
 
 export default function Page() {
-	const [type, toggleTyoe] = useReducer(
-		(state: EventType | '', action: EventType | '') => {
-			if (state === '') return action
-			if (state === action) return ''
-			else return action
-		},
-		''
-	)
+  const [type, toggleTyoe] = useReducer(
+    (state: EventType | "", action: EventType | "") => {
+      if (state === "") return action;
+      if (state === action) return "";
+      else return action;
+    },
+    ""
+  );
 
-	return (
-		<div className="flex flex-col gap-6 overflow-hidden">
-			<Title content="投稿頁面" />
+  return (
+    <div className="flex flex-col gap-6 overflow-hidden">
+      <Title content="投稿頁面" />
 
-			{/* count down */}
-			<Countdown />
-			{/* submit botton */}
-			<div className="flex justify-center">
-				<button className="bg-gold text-xl font-bold text-white h-[60px] w-[196px] rounded-full hover:shadow-[0px_4px_16px_0px_#E5C366CC] active:bg-[#D6A41D] active:shadow-[0px_2px_4px_0px_#E5C36699] focus:border focus:border-purple disabled:text-4-6 disabled:bg-2-6">
-					立刻投稿
-				</button>
-			</div>
+      {/* count down */}
+      <Countdown />
+      {/* submit botton */}
+      <div className="flex justify-center">
+        <button className="bg-gold text-xl font-bold text-white h-[60px] w-[196px] rounded-full hover:shadow-[0px_4px_16px_0px_#E5C366CC] active:bg-[#D6A41D] active:shadow-[0px_2px_4px_0px_#E5C36699] focus:border focus:border-purple disabled:text-4-6 disabled:bg-2-6">
+          立刻投稿
+        </button>
+      </div>
 
-			{/* time table */}
-			<div className="flex flex-col gap-4 pt-16">
-				<div className="flex items-center gap-6 flex-wrap max-w-[512px]">
-					<div>
-						<h2 className="text-[32px] font-medium">時程表</h2>
-					</div>
-					<div className="flex grow place-content-between">
-						<button
-							className={`rounded-full w-[110px] h-[40px] flex justify-center items-center transition-colors border border-purple ${
-								type === 'general'
-									? 'text-purple bg-white'
-									: 'text-white bg-purple'
-							}`}
-							onClick={() => toggleTyoe('general')}
-						>
-							一般議程
-						</button>
-						<button
-							className={`rounded-full w-[110px] h-[40px] flex justify-center items-center transition-colors border border-pink ${
-								type === 'undefined'
-									? 'text-pink bg-white'
-									: 'text-white bg-pink'
-							}`}
-							onClick={() => toggleTyoe('undefined')}
-						>
-							開放式議程
-						</button>
-						<button
-							className={`rounded-full w-[110px] h-[40px] flex justify-center items-center transition-colors border border-green ${
-								type === 'poster'
-									? 'text-green bg-white'
-									: 'text-white bg-green'
-							}`}
-							onClick={() => toggleTyoe('poster')}
-						>
-							海報
-						</button>
-					</div>
-				</div>
+      {/* time table */}
+      <div className="flex flex-col gap-4 pt-16">
+        <div className="flex items-center gap-6 flex-wrap max-w-[512px]">
+          <div>
+            <h2 className="text-[32px] font-medium">時程表</h2>
+          </div>
+          <div className="flex grow place-content-between">
+            <button
+              className={`rounded-full w-[110px] h-[40px] flex justify-center items-center transition-colors border border-purple ${
+                type === "general"
+                  ? "text-purple bg-white"
+                  : "text-white bg-purple"
+              }`}
+              onClick={() => toggleTyoe("general")}
+            >
+              一般議程
+            </button>
+            <button
+              className={`rounded-full w-[110px] h-[40px] flex justify-center items-center transition-colors border border-pink ${
+                type === "undefined"
+                  ? "text-pink bg-white"
+                  : "text-white bg-pink"
+              }`}
+              onClick={() => toggleTyoe("undefined")}
+            >
+              開放式議程
+            </button>
+            <button
+              className={`rounded-full w-[110px] h-[40px] flex justify-center items-center transition-colors border border-green ${
+                type === "poster"
+                  ? "text-green bg-white"
+                  : "text-white bg-green"
+              }`}
+              onClick={() => toggleTyoe("poster")}
+            >
+              海報
+            </button>
+          </div>
+        </div>
 
-				<div>
-					<Event
-						time={[2023, 'Nov.', 19]}
-						type={['general', 'undefined', 'poster']}
-						selectedType={type}
-						name="開始徵稿"
-					/>
-					<Event
-						time={[2024, 'Jan.', 20]}
-						type={['general', 'undefined', 'poster']}
-						selectedType={type}
-						name="投稿截止"
-						desc="至 2024 年 1 月 20 日（六）23:59"
-					/>
-					<Event
-						time={[2024, 'Jan.', '下旬']}
-						type={['general', 'undefined', 'poster']}
-						selectedType={type}
-						name="稿件錄取通知"
-					/>
-					<Event
-						time={[2024, 'Jan.', '下旬']}
-						endTime={[2024, 'Feb.', '中旬']}
-						type={['general', 'undefined']}
-						selectedType={type}
-						name="試講"
-						desc="若稿件入選，確切時間將再與您協調"
-					/>
-					<Event
-						time={[2024, 'Feb.', 18]}
-						type={['poster']}
-						selectedType={type}
-						name="錄取海報檔案上傳截止"
-					/>
-					<Event
-						time={[2024, 'Mar.', 8]}
-						type={['general', 'undefined']}
-						selectedType={type}
-						name="彩排"
-					/>
-					<Event
-						time={[2024, 'Mar.', 9]}
-						type={['SITCON']}
-						selectedType={type}
-						name="年會"
-					/>
-				</div>
-			</div>
+        <div>
+          <Event
+            time={[2023, "Nov.", 19]}
+            type={["general", "undefined", "poster"]}
+            selectedType={type}
+            name="開始徵稿"
+          />
+          <Event
+            time={[2024, "Jan.", 20]}
+            type={["general", "undefined", "poster"]}
+            selectedType={type}
+            name="投稿截止"
+            desc="至 2024 年 1 月 20 日（六）23:59"
+          />
+          <Event
+            time={[2024, "Jan.", "下旬"]}
+            type={["general", "undefined", "poster"]}
+            selectedType={type}
+            name="稿件錄取通知"
+          />
+          <Event
+            time={[2024, "Jan.", "下旬"]}
+            endTime={[2024, "Feb.", "中旬"]}
+            type={["general", "undefined"]}
+            selectedType={type}
+            name="試講"
+            desc="若稿件入選，確切時間將再與您協調"
+          />
+          <Event
+            time={[2024, "Feb.", 18]}
+            type={["poster"]}
+            selectedType={type}
+            name="錄取海報檔案上傳截止"
+          />
+          <Event
+            time={[2024, "Mar.", 8]}
+            type={["general", "undefined"]}
+            selectedType={type}
+            name="彩排"
+          />
+          <Event
+            time={[2024, "Mar.", 9]}
+            type={["SITCON"]}
+            selectedType={type}
+            name="年會"
+          />
+        </div>
+      </div>
 
-			{/* hero bg image */}
-			<div className="w-[calc(100vw-70%)] xl:w-[calc(100vw-60%)] h-[900px] overflow-hidden top-0 left-0 mt-[920px] ml-[70%] xl:ml-[60%] absolute -z-10 hidden lg:block">
-				<Image
-					src="/hero_bg.webp"
-					width={1857}
-					height={1998}
-					alt=""
-					className="max-w-[unset]"
-				/>
-			</div>
-		</div>
-	)
+      {/* hero bg image */}
+      <div className="w-[calc(100vw-70%)] xl:w-[calc(100vw-60%)] h-[900px] overflow-hidden top-0 left-0 mt-[920px] ml-[70%] xl:ml-[60%] absolute -z-10 hidden lg:block">
+        <Image
+          src="/hero_bg.webp"
+          width={1857}
+          height={1998}
+          alt=""
+          className="max-w-[unset]"
+        />
+      </div>
+    </div>
+  );
 }
--- components/cfp/Card.tsx
+++ components/cfp/Card.tsx
@@ -1,149 +1,149 @@
-'use client'
-import { useLayoutEffect, useState } from 'react'
+"use client";
+import { useLayoutEffect, useState } from "react";
 
 type Card = {
-    title: string
-    content: string
-}
+  title: string;
+  content: string;
+};
 
 type CardsProps = {
-    data: Card[]
-}
+  data: Card[];
+};
 
 export default function Cards({ data }: CardsProps) {
-    const [isDesktop, setIsDesktop] = useState(false)
+  const [isDesktop, setIsDesktop] = useState(false);
 
-    const varients = ['pink', 'purple', 'blue'] as const
-    useLayoutEffect(() => {
-        function update() {
-            setIsDesktop(window.innerWidth > 1024)
-        }
+  const varients = ["pink", "purple", "blue"] as const;
+  useLayoutEffect(() => {
+    function update() {
+      setIsDesktop(window.innerWidth > 1024);
+    }
 
-        window.addEventListener('resize', update)
-        update()
+    window.addEventListener("resize", update);
+    update();
 
-        return () => {
-            window.removeEventListener('resize', update)
-        }
-    }, [])
+    return () => {
+      window.removeEventListener("resize", update);
+    };
+  }, []);
 
-    // TODO: style
-    return (
-        <div className="grid grid-rows-3 gap-3 lg:grid-cols-3 lg:grid-rows-1 lg:gap-[14px] my-10">
-            {data.map((item, index) =>
-                isDesktop ? (
-                    <CardDesktop
-                        key={index}
-                        {...item}
-                        varient={varients[index % 3]}
-                    />
-                ) : (
-                    <Card key={index} {...item} varient={varients[index % 3]} />
-                )
-            )}
-        </div>
-    )
+  // TODO: style
+  return (
+    <div className="grid grid-rows-3 gap-3 lg:grid-cols-3 lg:grid-rows-1 lg:gap-[14px] my-10">
+      {data.map((item, index) =>
+        isDesktop ? (
+          <CardDesktop key={index} {...item} varient={varients[index % 3]} />
+        ) : (
+          <Card key={index} {...item} varient={varients[index % 3]} />
+        )
+      )}
+    </div>
+  );
 }
 
 const varients = {
-    pink: {
-        bg: 'bg-pink',
-        text: 'text-pink',
-        border: 'border-pink',
-        shadow: 'shadow-[0px_4px_4px_0px_#FF349580]',
-    },
-    purple: {
-        bg: 'bg-purple',
-        text: 'text-purple',
-        border: 'border-purple',
-        shadow: 'shadow-[0px_4px_4px_0px_#AC24FF80]',
-    },
-    blue: {
-        bg: 'bg-blue',
-        text: 'text-blue',
-        border: 'border-blue',
-        shadow: 'shadow-[0px_4px_4px_0px_#0CE29580]',
-    },
-}
+  pink: {
+    bg: "bg-pink",
+    text: "text-pink",
+    border: "border-pink",
+    shadow: "shadow-[0px_4px_4px_0px_#FF349580]",
+  },
+  purple: {
+    bg: "bg-purple",
+    text: "text-purple",
+    border: "border-purple",
+    shadow: "shadow-[0px_4px_4px_0px_#AC24FF80]",
+  },
+  blue: {
+    bg: "bg-blue",
+    text: "text-blue",
+    border: "border-blue",
+    shadow: "shadow-[0px_4px_4px_0px_#0CE29580]",
+  },
+};
 
 export function Card({
-    title,
-    content,
-    varient,
+  title,
+  content,
+  varient,
 }: Card & {
-    varient: 'pink' | 'purple' | 'blue'
+  varient: "pink" | "purple" | "blue";
 }) {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-    return (
-        <div
-            className="h-[200px] relative flex justify-center"
-            onMouseOver={() => setOpen(true)}
-            onMouseOut={() => setOpen(false)}
+  return (
+    <div
+      className="h-[200px] relative flex justify-center"
+      onMouseOver={() => setOpen(true)}
+      onMouseOut={() => setOpen(false)}
+    >
+      <div
+        className={`h-[200px] rounded-r-lg border ${varients[varient].border} ${
+          open
+            ? `w-[400px] translate-x-[100px] py-[10px] px-[40px] ${varients[varient].shadow}`
+            : "w-[300px] translate-x-[24px]"
+        } ${!open && varients[varient].bg} transition-all`}
+      >
+        <p className={`text-[14px] mt-4 ${varients[varient].text}`}>
+          {open && content}
+        </p>
+      </div>
+      <div
+        className={`h-[200px] absolute z-10 ${
+          open
+            ? `${varients[varient].bg} ${varients[varient].shadow}`
+            : "bg-gold"
+        } ${
+          open
+            ? "-translate-x-[198px] w-[200px] rounded-l-lg"
+            : "w-[300px] rounded-lg"
+        } transition-all flex justify-center items-center`}
+      >
+        <h1
+          className={`text-white font-bold ${
+            open ? "text-2xl" : "text-[64px]"
+          } transition-all leading-[93px]`}
         >
-            <div
-                className={`h-[200px] rounded-r-lg border ${varients[varient].border
-                    } ${open
-                        ? `w-[400px] translate-x-[100px] py-[10px] px-[40px] ${varients[varient].shadow}`
-                        : 'w-[300px] translate-x-[24px]'
-                    } ${!open && varients[varient].bg} transition-all`}
-            >
-                <p className={`text-[14px] mt-4 ${varients[varient].text}`}>
-                    {open && content}
-                </p>
-            </div>
-            <div
-                className={`h-[200px] absolute z-10 ${open
-                    ? `${varients[varient].bg} ${varients[varient].shadow}`
-                    : 'bg-gold'
-                    } ${open
-                        ? '-translate-x-[198px] w-[200px] rounded-l-lg'
-                        : 'w-[300px] rounded-lg'
-                    } transition-all flex justify-center items-center`}
-            >
-                <h1
-                    className={`text-white font-bold ${open ? 'text-2xl' : 'text-[64px]'
-                        } transition-all leading-[93px]`}
-                >
-                    {title}
-                </h1>
-            </div>
-        </div>
-    )
+          {title}
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export function CardDesktop({
-    title,
-    content,
-    varient,
+  title,
+  content,
+  varient,
 }: Card & {
-    varient: 'pink' | 'purple' | 'blue'
+  varient: "pink" | "purple" | "blue";
 }) {
-    const [open, setOpen] = useState(false)
-    return (
-        <div
-            className="h-[410px] relative overflow-y-hidden"
-            onMouseOver={() => setOpen(true)}
-            onMouseOut={() => setOpen(false)}
-        >
-            <div
-                className={`h-[400px] w-full absolute z-0 top-0 border rounded-lg py-5 px-10 ${varients[varient].text} ${varients[varient].border} ${varients[varient].shadow} `}
-            >
-                <h1 className="text-2xl text-center font-bold my-4">{title}</h1>
-                {content.split('\n').map((item, index) => (
-                    <p className="text-[15px]" key={index}>
-                        {item}
-                    </p>
-                ))}
-            </div>
-            <div
-                className={`h-[400px] w-full p-12 rounded-lg absolute z-10 bg-4-6 ${open && '-translate-y-[400px]'
-                    } transition-all flex justify-center items-center shadow-[0px_4px_16px_0px_#E5C366CC]`}
-            >
-                <h1 className="text-white text-center font-bold text-[64px] leading-[93px]">
-                    {title}
-                </h1>
-            </div>
-        </div>
-    )
+  const [open, setOpen] = useState(false);
+  return (
+    <div
+      className="h-[410px] relative overflow-y-hidden"
+      onMouseOver={() => setOpen(true)}
+      onMouseOut={() => setOpen(false)}
+    >
+      <div
+        className={`h-[400px] w-full absolute z-0 top-0 border rounded-lg py-5 px-10 ${varients[varient].text} ${varients[varient].border} ${varients[varient].shadow} `}
+      >
+        <h1 className="text-2xl text-center font-bold my-4">{title}</h1>
+        {content.split("\n").map((item, index) => (
+          <p className="text-[15px]" key={index}>
+            {item}
+          </p>
+        ))}
+      </div>
+      <div
+        className={`h-[400px] w-full p-12 rounded-lg absolute z-10 bg-4-6 ${
+          open && "-translate-y-[400px]"
+        } transition-all flex justify-center items-center shadow-[0px_4px_16px_0px_#E5C366CC]`}
+      >
+        <h1 className="text-white text-center font-bold text-[64px] leading-[93px]">
+          {title}
+        </h1>
+      </div>
+    </div>
+  );
 }
