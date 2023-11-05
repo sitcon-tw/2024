8fa45da00170a4762e5a7274a1f3aa5ad50e4970
gnehs <jayuiop@gmail.com>
2023-11-06 01:37:42 +0800

    updated favicon

--- app/cfp/layout.tsx
+++ app/cfp/layout.tsx
@@ -5,6 +5,10 @@ import BackToTopButton from "@/components/cfp/BackToTopBtn";
 export default function CFPLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
+      <head>
+        <link rel="icon" href="/cfp/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/cfp/favicon.png" type="image/png" />
+      </head>
       <Nav />
       <main className="flex-grow">
         {children}
