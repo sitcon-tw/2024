c3cb27aca87c4922ff37c60ffd83e46a4144f243
RedFeaTherXll <yuchi9716@gmail.com>
2023-11-05 21:00:10 +0800

    adjust subtitle component

--- components/cfp/Subtitle.tsx
+++ components/cfp/Subtitle.tsx
@@ -1,5 +1,5 @@
-export default function Subtitle({ children }: { children: React.ReactNode }) {
+export default function Subtitle({ children, id }: { children: React.ReactNode, id?: string}) {
   return (
-    <h2 className="text-2xl lg:text-[32px] font-bold mt-8 mb-4">{children}</h2>
+    <h2 id={id} className="text-2xl lg:text-[32px] font-bold mt-8 mb-4">{children}</h2>
   );
 }
