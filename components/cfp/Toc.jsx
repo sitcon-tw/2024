dfc637868e13b52f60d17f631180b1d9705c6cb8
SimbaFs <simba.fs@gmail.com>
2023-11-06 14:27:16 +0800

    add submit buttom to toc

--- components/cfp/Toc.jsx
+++ components/cfp/Toc.jsx
@@ -19,14 +19,16 @@ export default function Toc({ sections }) {
   };
 
   return (
-    <div className="flex gap-6 container">
-      <div className="min-w-[180px] hidden lg:flex flex-col p-4 fixed top-0 right-4 mt-32 bg-white hover:cursor-pointer">
-        <h2 className="text-xl font-bold mb-4">本頁目錄</h2>
+      <div className="lg:w-[143px] xl:w-[196px] hidden lg:flex flex-col p-4 fixed top-0 right-4 mt-24 bg-white hover:cursor-pointer gap-4">
+        <button className="bg-gold text-xl font-bold text-white h-[40px] w-full rounded-full hover:shadow-[0px_4px_16px_0px_#E5C366CC] active:bg-[#D6A41D] active:shadow-[0px_2px_4px_0px_#E5C36699] focus:border focus:border-purple disabled:text-4-6 disabled:bg-2-6">
+          立刻投稿
+        </button>
+        <h2 className="text-xl font-bold">本頁目錄</h2>
         <ul>
           {sections.map((section, index) => (
             <li key={index}>
               <a
-                className={`block mb-2 ${section.link === selectedSection ? "text-black font-extrabold " : "text-gray-500"}`}
+                className={`block ${section.link === selectedSection ? "text-black font-extrabold " : "text-gray-500"}`}
                 onClick={() => handleClick(section.link)}
               >
                 <div className="w-1 h-1.2em rounded-full bg-[#AC24FF] transition-all duration-300" />
@@ -36,6 +38,5 @@ export default function Toc({ sections }) {
           ))}
         </ul>
       </div>
-    </div>
   );
 }
