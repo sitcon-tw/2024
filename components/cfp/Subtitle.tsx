30d747b5e7a51c376663cfc1c0f1a31a4cda2ff3
SimbaFs <simba.fs@gmail.com>
2023-11-06 21:43:06 +0800

    update component Subtitle

--- components/cfp/Subtitle.tsx
+++ components/cfp/Subtitle.tsx
@@ -1,5 +1,5 @@
 export default function Subtitle({ children, id }: { children: React.ReactNode, id?: string}) {
   return (
-    <h2 id={id} className="text-2xl lg:text-[32px] font-bold pt-[80px] mb-4">{children}</h2>
+    <h2 id={id} className="text-2xl lg:text-[32px] font-bold my-4 scroll-mt-20">{children}</h2>
   );
 }
