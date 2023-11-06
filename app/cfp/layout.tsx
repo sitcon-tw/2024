e214b243f0ca5663847b45e43711ad83d310455c
SimbaFs <simba.fs@gmail.com>
2023-11-06 13:47:33 +0800

    fix hydration error

--- app/cfp/layout.tsx
+++ app/cfp/layout.tsx
@@ -2,13 +2,15 @@
 import Nav from "@/components/cfp/Nav";
 import Footer from "@/components/cfp/Footer";
 import BackToTopButton from "@/components/cfp/BackToTopBtn";
+import Head from 'next/head'
+
 export default function CFPLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <head>
+      <Head>
         <link rel="icon" href="/cfp/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/cfp/favicon.png" type="image/png" />
-      </head>
+      </Head>
       <Nav />
       <main className="flex-grow">
         {children}
