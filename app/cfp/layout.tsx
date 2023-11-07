437943c12b929229840f2ead743e575555b6285e
gnehs <jayuiop@gmail.com>
2023-11-07 15:53:43 +0800

    fixed icon

--- app/cfp/layout.tsx
+++ app/cfp/layout.tsx
@@ -2,15 +2,14 @@
 import Nav from "@/components/cfp/Nav";
 import Footer from "@/components/cfp/Footer";
 import BackToTopButton from "@/components/cfp/BackToTopBtn";
-import Head from 'next/head'
 
 export default function CFPLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Head>
+      <head>
         <link rel="icon" href="/cfp/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/cfp/favicon.png" type="image/png" />
-      </Head>
+      </head>
       <Nav />
       <main className="flex-grow">
         {children}
