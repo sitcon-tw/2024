8bd29c9143ed73ea91fd14bd8c2838ab521148c7
gnehs <jayuiop@gmail.com>
2023-11-11 01:19:44 +0800

    updated visual

--- components/cfp/Visual.tsx
+++ components/cfp/Visual.tsx
@@ -19,21 +19,21 @@ export default function Visual() {
   const { days, hours, minutes, seconds } = useCountdown();
   return (
     <div
-      className="h-[calc(100svh-64px)] min-h-[768px] bg-white bg-cover bg-[left_20%_center] lg:bg-center"
+      className="h-[calc(100svh-64px)] min-h-[614px] bg-white bg-cover bg-[left_20%_center] lg:bg-center"
       style={{
         backgroundImage: `url('/2024/cfp/intro.webp')`,
       }}
     >
-      <div className="py-4 flex flex-col gap-4 relative container h-full">
+      <div className="pb-4 flex flex-col gap-4 relative container h-full">
         <div className="lg:absolute top-8 left-8 flex items-start justify-center flex-col">
-          <div className="text-[128px] font-black leading-[1em] lg:text-[256px]">
+          <div className="text-[96px] font-black leading-[1em] lg:text-[256px]">
             CFP
           </div>
           <div className="tracking-[.5em] lg:tracking-[1em] lg:text-[24px]">
             CALL FOR PAPER
           </div>
         </div>
-        <div className="flex flex-col tracking-widest mt-16 lg:absolute bottom-8 left-8 lg:text-[24px] lg:mt-0 drop-shadow-[0_0_8px_rgba(255,255,255,0.4)]">
+        <div className="flex flex-col tracking-widest mt-4 lg:absolute bottom-8 left-8 lg:text-[24px] lg:mt-0 drop-shadow-[0_0_8px_rgba(255,255,255,0.4)]">
           <div>SITCON 2024</div>
           <div className="text-[2.5em] leading-[1.25em]">03.09</div>
           <div className="border-t-2 border-[#1E1E1E] pt-[0.5em] w-max">
@@ -44,7 +44,7 @@ export default function Visual() {
         </div>
         <div className="flex-grow" />
         <div className="lg:absolute right-8 bottom-8 lg:w-[350px] flex flex-col gap-4 drop-shadow-[0_0_8px_rgba(255,255,255,0.4)]">
-          <div className="border-l-2 border-r-2 border-[#1E1E1E] text-2xl text-center mb-20 lg:mb-8 w-max self-center px-4  lg:text-[32px] lg:tracking-widest lg:w-full">
+          <div className="border-l-2 border-r-2 border-[#1E1E1E] text-2xl text-center mb-4 lg:mb-8 w-max self-center px-4  lg:text-[32px] lg:tracking-widest lg:w-full">
             稿件徵求中
           </div>
           <div className="text-sm lg:text-base text-center">
