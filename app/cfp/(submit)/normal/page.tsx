b47c85b42c86e05cb441b957ae7967a0f40529be
gnehs <jayuiop@gmail.com>
2023-11-12 14:35:08 +0800

    updated normal page

--- app/cfp/(submit)/normal/page.tsx
+++ app/cfp/(submit)/normal/page.tsx
@@ -191,9 +191,9 @@ export default function Page() {
             {/* purple */}
             <p>
               Presentation <span className="text-purple"> 共 40 分鐘</span>
-              ，包含完整 30 分鐘的議程與 10 分鐘的問答時間，
+              ，包含完整 30 分鐘的議程與 10
+              分鐘的問答時間，讓您有充分的時間展示想法、描述經歷、和與會者交流
             </p>
-            <p>讓您有充分的時間展示想法、描述經歷、和與會者交流</p>
           </div>
           {/* <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-8 mt-4">
             <VideoCard href="#" image="test.jpg" title="test" />
