46ec86731e8e04c7331e5b782c0ed580334037c9
SimbaFs <simba.fs@gmail.com>
2023-11-13 23:34:50 +0800

    add link to notice page

--- app/cfp/(submit)/notice/page.tsx
+++ app/cfp/(submit)/notice/page.tsx
@@ -72,7 +72,7 @@ export default function Page() {
           的 SITCON 頻道。您為議程準備的其餘相關素材（如投影片），將於年會後由
           SITCON
           議程組向您蒐集素材連結，並整理在年會官方網站的議程資訊上。您可以自行決定在何種平台、以何種方式釋出，我們不會干涉，但鼓勵您選擇允許知識共享的
-          創用 CC 系列授權。
+          <a href="http://creativecommons.tw/explore" className="link" target="_blank">創用 CC 系列授權</a>。
         </p>
       </div>
     </div>
