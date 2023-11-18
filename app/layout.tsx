6577276301610d2577d445e6053e7ba0a3123cea
gnehs <jayuiop@gmail.com>
2023-11-18 16:51:26 +0800

    updated gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,16 +60,15 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+TC:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-GVRT4CHSQJ"
-        ></script>
         <Script
           id="gtm"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-GVRT4CHSQJ');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NPVBCDZ');`,
           }}
         />
         <Script
