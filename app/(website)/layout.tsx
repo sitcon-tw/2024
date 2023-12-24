689bdbebae0042969df02768a0969a7759e6c5b9
SimbaFs <simba.fs@gmail.com>
2023-12-24 19:03:48 +0800

    place footer to bottom

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -3,9 +3,13 @@ import Footer from "@/components/Footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Nav />
-      {children}
-      <Footer />
+      <div className="h-screen flex flex-col">
+        <Nav />
+        <div className="grow" >
+          {children}
+        </div>
+        <Footer />
+      </div>
     </>
   );
 }
