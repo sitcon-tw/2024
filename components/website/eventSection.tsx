2b344686cc2993158d299667a4fa38a5142e7852
Dada878 <xx0932399@gmail.com>
2023-12-29 18:23:54 +0800

    updated eventSection component

--- components/website/eventSection.tsx
+++ components/website/eventSection.tsx
@@ -1,9 +1,17 @@
-export default function EventSection({ title, content }: { title: React.ReactNode, content: React.ReactNode }) {
+"use client";
+export default function EventSection({ title, content, backgroundImage, backgroundPosition }: { title: React.ReactNode, content: Array<string>, backgroundImage: string, backgroundPosition: string}) {
+    const backgroundStyle = {
+        backgroundImage: `url('/2024/website/${backgroundImage}')`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: `${backgroundPosition} center`,
+    };
     return (
-        <div className="mb-16">
+        <div className="mb-16" style={backgroundStyle}>
             <h3 className="font-bold text-3xl">{title}</h3>
             <div className="ml-20 mt-9">
-                {content}
+                {content.map((line, index) => (
+                    <p key={index}>{line}{index !== content.length - 1 && <span><br /><br /></span>}</p>
+                ))}
             </div>
         </div>
     );
