a5602140a7e07defe5500ade916d28c8a5dd9636
RedFeaTherXll <yuchi9716@gmail.com>
2023-12-30 21:30:06 +0800

    traffic title

--- app/(website)/traffic/page.tsx
+++ app/(website)/traffic/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-  return <p>交通</p>;
+  return <div className="container">
+    <h1 className="text-5xl font-bold mb-14">交通方式</h1>
+    <div className=" justify-center">
+      <h3 className="text-3xl font-bold text-center">中央研究院 人文社會科學館</h3>
+      <h3 className="text-xl font-normal mt-8 text-center">中央研究院 人文社會科學館</h3>
+    </div>
+
+  </div>
 }
