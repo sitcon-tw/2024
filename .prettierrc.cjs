9c57e7831a0b4e3fdf52b5e0478e37476b5c1add
gnehs <jayuiop@gmail.com>
2023-12-30 21:38:52 +0800

    update prettierrc

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,6 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-	plugins: ['prettier-plugin-tailwindcss'],
-    tabWidth: 2,
-	useTabs: false,
-}
-
+  plugins: ["prettier-plugin-tailwindcss"],
+  tabWidth: 2,
+  useTabs: false,
+};
