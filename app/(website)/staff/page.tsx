dc1c867d65fcebdb2fc4b36a998fa055c9a6633c
gnehs <jayuiop@gmail.com>
2023-12-31 20:54:08 +0800

    updated staff page & added toc component

--- app/(website)/staff/page.tsx
+++ app/(website)/staff/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import Button from "@/components/website/button";
+import TableOfContent from "@/components/TableOfContent";
 import staffData from "./staff.json";
 import { motion } from "framer-motion";
 export default function Page() {
   return (
-    <div className="container">
-      <h1 className="text-2xl md:text-4xl font-bold mb-4">SITCON 團隊</h1>
+    <TableOfContent>
+      <h1 className="text-2xl md:text-4xl font-bold mb-4" id="page-title">
+        SITCON 團隊
+      </h1>
       <p>
         SITCON
         年會每年皆是許多志工奉獻時間與精神所舉辦，沒有大家的付出，就不可能會有如此內容豐富且高品質的年會，
@@ -37,7 +40,12 @@ export default function Page() {
           }}
           viewport={{ once: true }}
         >
-          <h1 className="text-xl md:text-2xl font-bold">{group.group}</h1>
+          <h1
+            className="text-xl md:text-2xl font-bold scroll-m-[96px]"
+            id={group.group}
+          >
+            {group.group}
+          </h1>
           <p>{group.description}</p>
           <div className="grid grid-cols-[repeat(auto-fill,minmax(72px,1fr))] gap-x-6 md:gap-12 md:gap-y-6 mt-6">
             {group.staff.map((member, index) => (
@@ -50,13 +58,13 @@ export default function Page() {
                   className="w-full aspect-square rounded-full bg-white"
                   whileHover={{ scale: 1.1 }}
                 />
-                <h1 className="mt-2 leading-6">{member.name}</h1>
-                <h1 className="text-xs mt-1">{member.role}</h1>
+                <div className="mt-2 leading-6">{member.name}</div>
+                <div className="text-xs mt-1">{member.role}</div>
               </div>
             ))}
           </div>
         </motion.div>
       ))}
-    </div>
+    </TableOfContent>
   );
 }
---
+++ components/TableOfContent.tsx
@@ -0,0 +1,123 @@
+import { useEffect, useState } from "react";
+import {
+  useScroll,
+  useMotionValueEvent,
+  motion,
+  AnimatePresence,
+} from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import { IoChevronDownOutline, IoChevronUpOutline } from "react-icons/io5";
+export default function TableOfContent({
+  children,
+  selector = "h1, h2, h3, h4",
+}: {
+  children: React.ReactNode;
+  selector?: string;
+}) {
+  const [headings, setHeadings] = useState<
+    { id: string; text: string; level: number; active: boolean }[]
+  >([]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { scrollY } = useScroll();
+  useEffect(() => {
+    const elements = Array.from(document.querySelectorAll(selector)).map(
+      (elem) => ({
+        id: elem.id,
+        //@ts-ignore
+        text: elem.innerText,
+        level: Number(elem.nodeName.charAt(1)),
+        active: false,
+      }),
+    );
+    setHeadings(elements);
+  }, []);
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    let newHeadings = [...headings].map((heading) => ({
+      ...heading,
+      active: false,
+    }));
+    for (let heading of newHeadings) {
+      //@ts-ignore
+      if (latest <= document.getElementById(heading.id)?.offsetTop + 84) {
+        heading.active = true;
+        setHeadings(newHeadings);
+        return;
+      }
+    }
+  });
+  return (
+    <div className="container md:grid md:grid-cols-[150px_1fr] gap-8 relative">
+      <div className="sticky top-[84px] self-start hidden md:block">
+        <div className="font-bold text-lg">本頁目錄</div>
+        {headings.map((heading, index) => (
+          <a
+            href={`#${heading.id}`}
+            className={twMerge(
+              `ml-${heading.level - 2} block relative`,
+              heading.active ? "text-[#462002] font-bold" : "text-[#462002]/60",
+            )}
+            key={index}
+          >
+            {heading.text}
+            <AnimatePresence>
+              {heading.active && (
+                <motion.div
+                  initial={{
+                    opacity: 0,
+                    x: 1,
+                  }}
+                  animate={{
+                    opacity: 1,
+                    x: 0,
+                  }}
+                  exit={{
+                    opacity: 0,
+                    x: 1,
+                  }}
+                  className="absolute top-0 bottom-0 m-auto -left-3 w-[4px] h-[1em] bg-[#462002] rounded"
+                />
+              )}
+            </AnimatePresence>
+          </a>
+        ))}
+      </div>
+      <button
+        className={twMerge(
+          "md:hidden flex justify-between items-center border-2 border-[#462002] px-4 cursor-pointer relative w-full text-[#462002] mb-4",
+          isMenuOpen ? "rounded-t-lg" : "rounded-lg",
+        )}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        本頁目錄
+        {isMenuOpen ? <IoChevronUpOutline /> : <IoChevronDownOutline />}
+      </button>
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            className={twMerge(
+              "absolute bg-[#F8F3E8] w-[calc(100%-32px)] border-2 border-[#462002] rounded-b-lg px-4 -mt-[18px] overflow-hidden",
+            )}
+            initial={{ height: 0 }}
+            animate={{ height: "auto" }}
+            exit={{ height: 0 }}
+          >
+            {headings.map((heading, index) => (
+              <a
+                href={`#${heading.id}`}
+                className={twMerge(
+                  `ml-${heading.level - 2} block relative`,
+                  "text-[#462002]",
+                )}
+                key={index}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                {heading.text}
+              </a>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <div>{children}</div>
+    </div>
+  );
+}
