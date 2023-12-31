1a621c7e0cf02d6ce0c8435f28e8cca452bfbfe7
gnehs <jayuiop@gmail.com>
2023-12-31 19:08:06 +0800

    updated agenda & venue page

--- app/(website)/venue/page.tsx
+++ app/(website)/venue/page.tsx
@@ -1,3 +1,12 @@
+import { PiMapTrifoldDuotone } from "react-icons/pi";
 export default function Page() {
-  return <p>會場地圖</p>;
+  return (
+    <div className="container flex flex-col justify-center items-center text-center pt-20 gap-4">
+      <PiMapTrifoldDuotone className="text-8xl" />
+      <div>
+        <div className="text-4xl font-bold">會場地圖</div>
+        <div className="text-2xl">即將上線！</div>
+      </div>
+    </div>
+  );
 }
