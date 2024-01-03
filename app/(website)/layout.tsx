fc8b5b387ad386132c2d2bda345c63ae6e1b9dee
gnehs <jayuiop@gmail.com>
2024-01-03 20:54:28 +0800

    updated layout

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -2,12 +2,10 @@ import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <div className="flex min-h-screen flex-col bg-[#F8F3E8]">
-        <Nav />
-        <div className="grow">{children}</div>
-        <Footer />
-      </div>
-    </>
+    <div className="flex min-h-screen flex-col bg-[#F8F3E8]">
+      <Nav />
+      <div className="grow">{children}</div>
+      <Footer />
+    </div>
   );
 }
