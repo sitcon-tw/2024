5803a38daf8d7db38c59b7c417d70a2f959791d8
gnehs <jayuiop@gmail.com>
2024-01-03 20:54:38 +0800

    added sponsor dialog

--- app/(website)/sponsor/page.tsx
+++ app/(website)/sponsor/page.tsx
@@ -5,6 +5,8 @@ import Counter from "@/components/cfp/Counter";
 import TableOfContent from "@/components/TableOfContent";
 import sponsorData from "./sponsorData";
 import { twMerge } from "tailwind-merge";
+import Dialog from "@/components/website/Dialog";
+import { useState } from "react";
 function Sponsors({
   sponsors,
   largeSponsor = false,
@@ -22,14 +24,54 @@ function Sponsors({
       )}
     >
       {sponsors.map((sponsor, i) => (
-        <img
-          src={`/2024/sponsor/${sponsor.image}`}
-          className="mx-auto mb-4 w-full rounded"
-          alt={sponsor.title}
-          key={i}
-        />
+        <Sponsor sponsor={sponsor} key={i} />
       ))}
     </div>
+  );
+}
+function Sponsor({
+  sponsor,
+}: {
+  sponsor: (typeof sponsorData.specialThanks)[0];
+}) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  return (
+    <>
+      <img
+        src={`/2024/sponsor/${sponsor.image}`}
+        className="mx-auto w-full cursor-pointer rounded"
+        alt={sponsor.title}
+        onClick={() => setIsDialogOpen(true)}
+      />
+      <Dialog open={isDialogOpen} setOpen={setIsDialogOpen}>
+        <div className="flex flex-col gap-4 p-6">
+          <div className="flex items-center justify-between text-2xl font-bold md:text-3xl">
+            {sponsor.title}
+          </div>
+          <img
+            src={`/2024/sponsor/${sponsor.image}`}
+            className="mx-auto w-full max-w-[240px] rounded"
+            alt={sponsor.title}
+          />
+          <div>{sponsor.description}</div>
+          <div className="flex items-center justify-center gap-6">
+            <Button
+              color="blue"
+              url={sponsor.url}
+              className="text-base md:text-xl"
+            >
+              前往網站
+            </Button>
+          </div>
+        </div>
+        <div
+          className="h-4 w-full rounded-b-[10px]"
+          style={{
+            backgroundImage: `linear-gradient(90deg, rgba(56, 90, 172, 0.80) 0%, rgba(148, 110, 52, 0.75) 58.96%, #462002 100%)`,
+          }}
+        ></div>
+      </Dialog>
+    </>
   );
 }
 
