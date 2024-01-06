bc06ff84da57fe58b22891e033fd16f69f88a6db
gnehs <jayuiop@gmail.com>
2024-01-06 17:02:37 +0800

    Add mounted state and useEffect cleanup

--- components/website/Dialog.tsx
+++ components/website/Dialog.tsx
@@ -1,7 +1,8 @@
+"use client";
 import useMediaQuery from "@/hooks/use-media-query";
 import { Drawer } from "vaul";
 import { AnimatePresence, motion } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { IoCloseCircle } from "react-icons/io5";
 import { createPortal } from "react-dom";
 
@@ -14,7 +15,14 @@ export default function Dialog({
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
+  const [mounted, setMounted] = useState(false);
   const { isMobile } = useMediaQuery();
+
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
   useEffect(() => {
     if (!isMobile) {
       document.body.style.overflow = open ? "hidden" : "auto";
@@ -33,36 +41,38 @@ export default function Dialog({
       </Drawer.Root>
     );
   }
-  return createPortal(
-    <AnimatePresence>
-      {open && (
-        <div className="fixed inset-0 z-20">
-          <motion.div
-            className="fixed inset-0 cursor-pointer bg-black/40 backdrop-blur"
-            onClick={() => setOpen(false)}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          />
-          <motion.div
-            className="fixed inset-0 m-auto flex h-max w-[512px] flex-col rounded-[10px] bg-[#F8F3E8]"
-            initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, scale: 0.95 }}
-          >
-            <motion.button
-              onClick={() => setOpen(false)}
-              className="absolute right-0 top-0 m-4"
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-            >
-              <IoCloseCircle className="text-2xl text-[#385AAC]" />
-            </motion.button>
-            {children}
-          </motion.div>
-        </div>
-      )}
-    </AnimatePresence>,
-    document.body,
-  );
+  return mounted
+    ? createPortal(
+        <AnimatePresence>
+          {open && (
+            <div className="fixed inset-0 z-20">
+              <motion.div
+                className="fixed inset-0 cursor-pointer bg-black/40 backdrop-blur"
+                onClick={() => setOpen(false)}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              />
+              <motion.div
+                className="fixed inset-0 m-auto flex h-max w-[512px] flex-col rounded-[10px] bg-[#F8F3E8]"
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, scale: 0.95 }}
+              >
+                <motion.button
+                  onClick={() => setOpen(false)}
+                  className="absolute right-0 top-0 m-4"
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  <IoCloseCircle className="text-2xl text-[#385AAC]" />
+                </motion.button>
+                {children}
+              </motion.div>
+            </div>
+          )}
+        </AnimatePresence>,
+        document.body,
+      )
+    : null;
 }
