9c002c8881542df3c4687f8cd322665be2f96e28
gnehs <jayuiop@gmail.com>
2024-01-12 11:11:18 +0800

    updated staff page

--- app/(website)/staff/page.tsx
+++ app/(website)/staff/page.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 export default function Page() {
   return (
     <TableOfContent>
-      <h1 className="text-2xl md:text-4xl font-bold mb-4" id="page-title">
+      <h1 className="mb-4 text-2xl font-bold md:text-4xl" id="page-title">
         SITCON 團隊
       </h1>
       <p>
@@ -18,7 +18,7 @@ export default function Page() {
         若你對參與 SITCON 年會的籌備有興趣，歡迎填寫跳坑表單或是關注 SITCON
         的郵件論壇，明年的籌備開始時，你就會收到相關的資訊！
       </p>
-      <div className="flex justify-center items-center gap-6 mt-6">
+      <div className="mt-6 flex items-center justify-center gap-6">
         <Button
           color="blue"
           url="https://docs.google.com/forms/d/e/1FAIpQLSfZHHumwRKY9UzBpMUD7MWzimnOLvm-cPu4noIzyoWPHbaaHA/viewform"
@@ -41,25 +41,25 @@ export default function Page() {
           viewport={{ once: true }}
         >
           <h1
-            className="text-xl md:text-2xl font-bold scroll-m-[96px]"
+            className="scroll-m-[96px] text-xl font-bold md:text-2xl"
             id={group.group}
           >
             {group.group}
           </h1>
           <p>{group.description}</p>
-          <div className="grid grid-cols-[repeat(auto-fill,minmax(72px,1fr))] gap-6 md:gap-12 md:gap-y-6 mt-6">
+          <div className="mt-6 grid grid-cols-[repeat(auto-fill,minmax(72px,1fr))] gap-6 md:gap-12 md:gap-y-6">
             {group.staff.map((member, index) => (
               <div
                 key={index}
                 className="flex flex-col items-center justify-start text-center"
               >
                 <motion.img
-                  src={`https://www.gravatar.com/avatar/${member.email}?s=512`}
-                  className="w-full aspect-square rounded-full bg-white"
+                  src={`https://www.gravatar.com/avatar/${member.email}?s=512&d=https://sitcon.org/2022/imgs/deafult_avatar.jpg&r=g`}
+                  className="aspect-square w-full rounded-full bg-white"
                   whileHover={{ scale: 1.1 }}
                 />
                 <div className="mt-2 leading-6">{member.name}</div>
-                <div className="text-xs mt-1">{member.role}</div>
+                <div className="mt-1 text-xs">{member.role}</div>
               </div>
             ))}
           </div>
