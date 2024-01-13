934214cbbaaacbb76d890fe1107527e56000e207
gnehs <jayuiop@gmail.com>
2024-01-14 00:43:03 +0800

    added theme-color

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -2,10 +2,15 @@ import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col bg-[#F8F3E8]">
-      <Nav />
-      <div className="grow">{children}</div>
-      <Footer />
-    </div>
+    <>
+      <head>
+        <meta name="theme-color" content="#F8F3E8" />
+      </head>
+      <div className="flex min-h-screen flex-col bg-[#F8F3E8]">
+        <Nav />
+        <div className="grow">{children}</div>
+        <Footer />
+      </div>
+    </>
   );
 }
