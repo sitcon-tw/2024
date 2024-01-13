1421cf8704ed5e8bbed1a7af17fa5760ebf63be2
gnehs <jayuiop@gmail.com>
2024-01-14 01:05:22 +0800

    Refactor website layout

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -2,15 +2,13 @@ import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="flex min-h-screen flex-col bg-[#F8F3E8]">
       <head>
         <meta name="theme-color" content="#F8F3E8" />
       </head>
-      <div className="flex min-h-screen flex-col bg-[#F8F3E8]">
-        <Nav />
-        <div className="grow">{children}</div>
-        <Footer />
-      </div>
-    </>
+      <Nav />
+      <div className="grow">{children}</div>
+      <Footer />
+    </div>
   );
 }
