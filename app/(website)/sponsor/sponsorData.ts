239f30aa22950384b3abda71b4e73b1bffa3dd28
arnoldsky-yu <99588albert@gmail.com>
2024-01-14 23:51:18 +0800

    update sponsorData.ts

--- app/(website)/sponsor/sponsorData.ts
+++ app/(website)/sponsor/sponsorData.ts
@@ -25,18 +25,18 @@ export default {
     },
   ],
   sponsors: [
-    //{
-    //  title: "領航級",
-    //  items: [
-    //    {
-    //      title: "LINE",
-    //      image: "line.png",
-    //      description:
-    //        "總部位於日本的LINE以「拉近你我的距離」為企業使命，透過多元的行動服務與內容，致力將人們、資訊與社會緊密連結。自2011年6月以通訊軟體的型態推出，現更推展其多元的全球生態系，並將觸角延伸至人工智慧、金融科技等領域，積極實現「智慧入口」的願景，2021年3月，LINE與日本規模最大的網路科技集團之一Z Holdings 正式整併，未來也將持續提供用戶享有最即時、便利的行動生活。",
-    //      url: "https://techblog.lycorp.co.jp/zh-hant/search?q=intern",
-    //    },
-    //  ],
-    //},
+    {
+     title: "領航級",
+     items: [
+       {
+         title: "LINE",
+         image: "line.png",
+         description:
+           "總部位於日本的LINE以「拉近你我的距離」為企業使命，透過多元的行動服務與內容，致力將人們、資訊與社會緊密連結。自2011年6月以通訊軟體的型態推出，現更推展其多元的全球生態系，並將觸角延伸至人工智慧、金融科技等領域，積極實現「智慧入口」的願景，2021年3月，LINE與日本規模最大的網路科技集團之一Z Holdings 正式整併，未來也將持續提供用戶享有最即時、便利的行動生活。",
+         url: "https://techblog.lycorp.co.jp/zh-hant/search?q=intern",
+       },
+     ],
+    },
     // {
     //   title: "深耕級",
     //   items: [],
