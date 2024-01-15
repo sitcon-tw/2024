89af4dd455b7e098e17085db9d0173b2cbb65ed4
陳柏宇 <84923484+arnoldsky-yu@users.noreply.github.com>
2024-01-15 14:06:54 +0800

    update sponsor
    * update sponsor logo
    
    * update sponsorData.ts
    
    * update sponsorData.ts

--- app/(website)/sponsor/sponsorData.ts
+++ app/(website)/sponsor/sponsorData.ts
@@ -26,16 +26,16 @@ export default {
   ],
   sponsors: [
     {
-      title: "領航級",
-      items: [
-        {
-          title: "LINE",
-          image: "LINE.png",
-          description:
-            "總部位於日本的LINE以「拉近你我的距離」為企業使命，透過多元的行動服務與內容，致力將人們、資訊與社會緊密連結。自2011年6月以通訊軟體的型態推出，現更推展其多元的全球生態系，並將觸角延伸至人工智慧、金融科技等領域，積極實現「智慧入口」的願景，2021年3月，LINE與日本規模最大的網路科技集團之一Z Holdings 正式整併，未來也將持續提供用戶享有最即時、便利的行動生活。",
-          url: "https://techblog.lycorp.co.jp/zh-hant/search?q=intern",
-        },
-      ],
+     title: "領航級",
+     items: [
+       {
+         title: "LINE",
+         image: "line.png",
+         description:
+           "總部位於日本的LINE以「拉近你我的距離」為企業使命，透過多元的行動服務與內容，致力將人們、資訊與社會緊密連結。自2011年6月以通訊軟體的型態推出，現更推展其多元的全球生態系，並將觸角延伸至人工智慧、金融科技等領域，積極實現「智慧入口」的願景，2021年3月，LINE與日本規模最大的網路科技集團之一Z Holdings 正式整併，未來也將持續提供用戶享有最即時、便利的行動生活。",
+         url: "https://techblog.lycorp.co.jp/zh-hant/search?q=intern",
+       },
+     ],
     },
     // {
     //   title: "深耕級",
@@ -44,34 +44,41 @@ export default {
     {
       title: "前瞻級",
       items: [
-        {
-          title: "曉數碼 Akatsuki Taiwan Inc.",
-          image: "akatsuki.png",
-          description:
-            "曉數碼為 Akatsuki Inc. 於 2014 年在台灣設立的子公司，以「與創作者共鳴，娛樂全世界」為使命，致力於開發、營運各種能躍動人心的遊戲。曉數碼重視在地化品質並放眼國際市場，團隊中聚集來自各地的成員，包含台、日、韓、英、法、德、西與印度等多語言人才。我們亦積極打造幸福職場，以此支持成員們盡情發揮創意，使其發光發熱，共同成長。",
-          url: "https://bit.ly/3bwhylS",
-        },
-        {
-          title: "Panasonic",
-          image: "Panasonic_b_posi.png",
-          description:
-            "Panasonic 致力於通過開發多元化的物聯網（IoT）產品，來讓人們的生活變得更美好。我們非常關注產品網路安全和客戶隱私，因此在日本總部不僅設立了 Product Security Center (PSC) 和 Product Security Incident Response Team (PSIRT)，還在台灣成立了 Panasonic Cyber Security Lab (PCSL)。PSC 和 PCSL 與全球各個業務集團合作，執行了需多的產品安全專案。這些專案包括對 IoT 產品網路威脅的深入研究、滲透測試、產品網路安全的事件響應，以及培養集團的網路安全人才。我們的目標是不斷增強和保護 Panasonic 產品的網路安全，確保我們的客戶獲得方便、安全的體驗。我們熱切歡迎有前瞻性的企業和優秀的個人加入我們的旅程，一同提升產品安全。如果您有合作意願，請隨時聯絡我們，Email 地址是 panasonic.pcsl@gmail.com。讓我們共同創建一個更安全、更緊密聯繫的世界！",
-          url: "https://sitcon.org",
-        },
+       {
+         title: "帕亞科技股份有限公司 & 臺科大營建系",
+         image: "paia-and-ntust-cce.png",
+         description:
+           "帕亞科技目前主力產品為 PAIA 遊戲AI競賽平台，我們打造了一個遊戲情境，讓使用者可以上傳各自的AI在平台上競賽，也提供積木式的開發工具，讓國中小學生可以輕易使用，學習AI。臺科大營建系資訊組以促進資通訊科技於土木營建與防災工程的全生命週期導入與應用為目標，朝向智慧營建及永續土木工程等方向發展。",
+         url: "https://www.paia-arena.com/",
+       },
+      //  {
+      //    title: "曉數碼 Akatsuki Taiwan Inc.",
+      //    image: "akatsuki.png",
+      //    description:
+      //      "曉數碼為 Akatsuki Inc. 於 2014 年在台灣設立的子公司，以「與創作者共鳴，娛樂全世界」為使命，致力於開發、營運各種能躍動人心的遊戲。曉數碼重視在地化品質並放眼國際市場，團隊中聚集來自各地的成員，包含台、日、韓、英、法、德、西與印度等多語言人才。我們亦積極打造幸福職場，以此支持成員們盡情發揮創意，使其發光發熱，共同成長。",
+      //    url: "https://bit.ly/3bwhylS",
+      //  },
+      //  {
+      //    title: "Panasonic",
+      //    image: "panasonic.png",
+      //    description:
+      //      "Panasonic 致力於通過開發多元化的物聯網（IoT）產品，來讓人們的生活變得更美好。我們非常關注產品網路安全和客戶隱私，因此在日本總部不僅設立了 Product Security Center (PSC) 和 Product Security Incident Response Team (PSIRT)，還在台灣成立了 Panasonic Cyber Security Lab (PCSL)。PSC 和 PCSL 與全球各個業務集團合作，執行了需多的產品安全專案。這些專案包括對 IoT 產品網路威脅的深入研究、滲透測試、產品網路安全的事件響應，以及培養集團的網路安全人才。我們的目標是不斷增強和保護 Panasonic 產品的網路安全，確保我們的客戶獲得方便、安全的體驗。我們熱切歡迎有前瞻性的企業和優秀的個人加入我們的旅程，一同提升產品安全。如果您有合作意願，請隨時聯絡我們，Email 地址是 panasonic.pcsl@gmail.com。讓我們共同創建一個更安全、更緊密聯繫的世界！",
+      //    url: "https://sitcon.org",
+      //  },
       ],
     },
-    {
-      title: "新芽級",
-      items: [
-        {
-          title: "ZUSO Generation 如梭世代",
-          image: "zuso.png",
-          description:
-            "ZUSO 如梭世代為臺灣本土的資安服務業者，提供紅隊演練、滲透測試及顧問服務與事件調查等服務。團隊在駭客攻擊手法研究和威脅風險解析上擁有超過10年的豐富經驗，致力為企業客戶提供客製化資安解決方案，期望透過最高標準的安全措施，協助各行業建立強固的安全防線。",
-          url: "https://zuso.ai",
-        },
-      ],
-    },
+    //{
+    //  title: "新芽級",
+    //  items: [
+    //    {
+    //      title: "ZUSO Generation 如梭世代",
+    //      image: "zuso.png",
+    //      description:
+    //        "ZUSO 如梭世代為臺灣本土的資安服務業者，提供紅隊演練、滲透測試及顧問服務與事件調查等服務。團隊在駭客攻擊手法研究和威脅風險解析上擁有超過10年的豐富經驗，致力為企業客戶提供客製化資安解決方案，期望透過最高標準的安全措施，協助各行業建立強固的安全防線。",
+    //      url: "https://zuso.ai",
+    //    },
+    //  ],
+    //},
   ],
   specialThanks: [
     // {
