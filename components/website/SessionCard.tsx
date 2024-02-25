d63181964e2b4dab1a96215056e65daff25c6310
gnehs <jayuiop@gmail.com>
2024-02-25 23:50:12 +0800

    fixed agenda timezone

--- components/website/SessionCard.tsx
+++ components/website/SessionCard.tsx
@@ -24,6 +24,7 @@ export default function SessionCard({
         hour: "2-digit",
         minute: "2-digit",
         hour12: false,
+        timeZone: "Asia/Taipei",
       })
       .replace(/\:/g, "");
   }
