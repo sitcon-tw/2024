35c00cb3ea20fe7278c6e039170cdc820c078a44
gnehs <jayuiop@gmail.com>
2024-03-03 14:10:25 +0800

    fixed esc close dialog

--- components/website/SessionDialog.tsx
+++ components/website/SessionDialog.tsx
@@ -1,7 +1,7 @@
 "use client";
 import useMediaQuery from "@/hooks/use-media-query";
 import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { IoCloseCircle } from "react-icons/io5";
 import { createPortal } from "react-dom";
 import { RemoveScroll } from "react-remove-scroll";
@@ -18,78 +18,62 @@ export default function Dialog({
   isOpenByDefault?: boolean;
 }) {
   const [mounted, setMounted] = useState(false);
-
+  const closeButtonRef = useRef<HTMLButtonElement>(null);
   useEffect(() => {
     setMounted(true);
-    return () => setMounted(false);
+    return () => {
+      setMounted(false);
+    };
   }, []);
-
-  let closed = false;
+  useEffect(() => {
+    if (closeButtonRef.current) {
+      closeButtonRef.current.focus();
+    }
+  }, [open]);
 
   function handleClose() {
     setOpen(false);
-    if (!closed) {
-    closed = true;
-    if (isOpenByDefault) {
-      history.pushState(
-        null,
-        "",
-        window.location.pathname.split("agenda").slice(0, -1).join("") +
-          "agenda/",
-      );
-    } else {
-      history.back();
-    }
-    }
   }
-
-  useEffect(() => {
-    const handleKeyUp = (event: KeyboardEvent) => {
-      if (event.keyCode === 27) {
-        handleClose();
-      }
-    };
-  
-    window.addEventListener('keyup', handleKeyUp);
-  
-    return () => {
-      window.removeEventListener('keyup', handleKeyUp);
-    };
-  }, []);
 
   return mounted
     ? createPortal(
-        <AnimatePresence>
-          {open && (
-            <RemoveScroll className="fixed inset-0 z-50 overflow-scroll">
-              <motion.div
-                className="fixed inset-0 cursor-pointer bg-black/50 backdrop-blur"
-                onClick={handleClose}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              />
-              <motion.div
-                className="fixed inset-0 m-auto flex w-screen flex-col overflow-y-scroll rounded-[10px] bg-transparent md:px-24"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, scale: 0.95 }}
-              >
-                <motion.button
+        <div>
+          <AnimatePresence>
+            {open && (
+              <RemoveScroll className="fixed inset-0 z-50 overflow-scroll">
+                <motion.div
+                  className="fixed inset-0 cursor-pointer bg-black/50 backdrop-blur"
                   onClick={handleClose}
-                  className="fixed right-2 top-2 m-2 text-3xl md:right-16 md:top-16"
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.9 }}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                />
+                <motion.div
+                  className="fixed inset-0 m-auto flex w-screen flex-col overflow-y-scroll rounded-[10px] bg-transparent md:px-24"
+                  initial={{ opacity: 0, y: 100 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, scale: 0.95 }}
                 >
-                  <IoCloseCircle className="text-3xl text-[#ffffff]" />
-                </motion.button>
-                <div className="max-md:p-4 max-md:pt-14 md:px-10 md:py-20">
-                  {children}
-                </div>
-              </motion.div>
-            </RemoveScroll>
-          )}
-        </AnimatePresence>,
+                  <motion.button
+                    onClick={handleClose}
+                    className="fixed right-2 top-2 m-2 text-3xl outline-0 md:right-16 md:top-16"
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    ref={closeButtonRef}
+                    onKeyDown={(e) => {
+                      if (e.key === "Escape") handleClose();
+                    }}
+                  >
+                    <IoCloseCircle className="text-3xl text-[#ffffff]" />
+                  </motion.button>
+                  <div className="max-md:p-4 max-md:pt-14 md:px-10 md:py-20">
+                    {children}
+                  </div>
+                </motion.div>
+              </RemoveScroll>
+            )}
+          </AnimatePresence>
+        </div>,
         document.body,
       )
     : null;
