2020cf1beb72e5128a384cf23cf9e5d09426df84
dada878 <dada878@users.noreply.github.com>
2024-12-01 00:09:36 +0800

    deploy: b148b3f747e5765b7ec5cdac58b8324418cd835a

---
+++ _next/static/chunks/app/(website)/agenda/[id]/page-d9cf1e842a12091b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9764],{},function(n){n.O(0,[3190,3676,5717,1750,1396,504,8021,3994,7835,5469,2971,2472,1744],function(){return n(n.s=5469)}),_N_E=n.O()}]);
